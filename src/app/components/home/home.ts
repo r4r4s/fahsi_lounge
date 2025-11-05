b85f4c74728d317876832ad7fbecc73d60abd5de
Rares <rngheru@gmail.com>
2025-11-05 22:22:52 +0100

    home arreglado

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router'; // <-- 1. IMPORTAR ESTO
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [
+    RouterLink // <-- 2. AÑADIR ESTO AQUÍ
+  ],
   templateUrl: './home.html',
-  styleUrl: './home.css',
+  styleUrl: './home.css'
 })
 export class Home {
 
