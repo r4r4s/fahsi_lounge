8da781bb3296651f992bbcd4f925a95b1995a996
Rares <rngheru@gmail.com>
2025-11-11 12:49:19 +0100

    Footer y cosas añadidas

--- src/app/services/producto.ts
+++ src/app/services/producto.ts
@@ -11,12 +11,8 @@ export class ProductoService {
   
   private productosMock: Producto[] = [
     
-    { id: 1, nombre: 'Cerveza Tostada', descripcion: 'Artesanal.', precio: 4.50, categoria: 'Cerveza' },
-    { id: 2, nombre: 'Cerveza Rubia', descripcion: 'Suave.', precio: 3.50, categoria: 'Cerveza' },
-    { id: 3, nombre: 'Cerveza Negra', descripcion: 'Intensa.', precio: 5.00, categoria: 'Cerveza' },
-    
-
-    
+    { id: 1, nombre: 'San Miguel', descripcion: 'Tercio', precio: 3.00, categoria: 'Cerveza' },
+    { id: 2, nombre: 'San Miguel', descripcion: 'Doble', precio: 2.50, categoria: 'Cerveza' },
     
     { id: 5, nombre: 'Gin Tonic (Beefeater)', descripcion: 'Clásico.', precio: 8.00, categoria: 'Copa' },
     { id: 6, nombre: 'Ron Cola (Barceló)', descripcion: 'El de siempre.', precio: 8.00, categoria: 'Copa' },
@@ -26,14 +22,16 @@ export class ProductoService {
     { id: 5, nombre: 'Copa de Champagne', descripcion: 'Burbujeante.', precio: 12.00, categoria: 'Copa' },
 
     
-    { id: 9, nombre: 'Cachimba Frutal', descripcion: 'Sabor a frutas mixtas.', precio: 15.00, categoria: 'Cachimba' },
-    { id: 10, nombre: 'Cachimba Menta', descripcion: 'Refrescante sabor a menta.', precio: 15.00, categoria: 'Cachimba' },
-    { id: 11, nombre: 'Cachimba Vainilla', descripcion: 'Dulce sabor a vainilla.', precio: 15.00, categoria: 'Cachimba' },
+    { id: 9, nombre: 'Mi amor', descripcion: 'Dulce, afrutado y suave', precio: 18.00, categoria: 'Cachimba' },
+    { id: 10, nombre: 'Menta', descripcion: 'Menta fresca', precio: 18.00, categoria: 'Cachimba' },
+    { id: 11, nombre: 'Doble manzana', descripcion: 'Anís y manzana intensa', precio: 18.00, categoria: 'Cachimba' },
+    { id: 12, nombre: 'Regaliz', descripcion: 'Intenso, dulce y especiado', precio: 18.00, categoria: 'Cachimba' },
 
     
-    { id: 12, nombre: 'Cartas', descripcion: 'Baraja española.', precio: 5.00, categoria: 'Juego' },
-    { id: 13, nombre: 'Dominó', descripcion: 'Juego clásico de fichas.', precio: 7.00 , categoria: 'Juego' },
-    { id: 14, nombre: 'Ajedrez', descripcion: 'Juego de estrategia.', precio: 10.00, categoria: 'Juego' },
+    { id: 13, nombre: 'Billar', descripcion: '', precio: 2.00, categoria: 'Juego' },
+    { id: 14, nombre: 'Diana', descripcion: '*1€ por persona', precio: 2.00, categoria: 'Juego' },
+    { id: 15, nombre: 'Futbolín', descripcion: '', precio: 1.00, categoria: 'Juego' },
+    
   ];
 
   constructor() { }
