9eb71573384ad9656513c6703f00eb146cd419c3
R4r4s <rngheru@gmail.com>
2025-11-24 22:31:36 +0100

    cookies

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './layout/header/header';
 import { Footer } from './layout/footer/footer';
+import { CookiesBanner } from './components/cookies-banner/cookies-banner';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header, Footer, CookiesBanner],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
