03dcbe1a37aa7089e550756e0a8d92abf2789c92
R4r4s <rngheru@gmail.com>
2025-12-09 19:19:28 +0100

    Página Promociones

--- src/app/components/promociones/promociones.ts
+++ src/app/components/promociones/promociones.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-promociones',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './promociones.html',
   styleUrl: './promociones.css'
 })
 export class Promociones {
+  // Imagenes Oferta
+  images: string[] = [
+    'assets/promociones/OfertaCopas.svg',
+    'assets/promociones/OfertaCopasCachimba.svg'
+    
+  ];
 
+  selectedImage: string | null = null;
+
+  openLightbox(image: string): void {
+    this.selectedImage = image;
+    document.body.style.overflow = 'hidden'; 
+  }
+
+  closeLightbox(): void {
+    this.selectedImage = null;
+    document.body.style.overflow = ''; 
+  }
 }
