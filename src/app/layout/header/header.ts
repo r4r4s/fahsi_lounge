5cf484d2bdd0b7807033661068293600a8a91fe2
R4r4s <rngheru@gmail.com>
2025-12-09 18:59:05 +0100

    Arreglos header y background

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild, HostListener, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { Collapse } from 'bootstrap';
 
@@ -13,11 +13,21 @@ import { Collapse } from 'bootstrap';
   styleUrls: ['./header.css']
 })
 export class Header {
+  private elementRef = inject(ElementRef);
+
   @ViewChild('navbarNav')
   private navbarNav?: ElementRef<HTMLElement>;
 
   @ViewChild('navbarToggler')
   private navbarToggler?: ElementRef<HTMLButtonElement>;
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent): void {
+    const clickedInside = this.elementRef.nativeElement.contains(event.target);
+    if (!clickedInside) {
+      this.closeNavbarOnMobile();
+    }
+  }
 
   closeNavbarOnMobile(): void {
     const collapseElement = this.navbarNav?.nativeElement;
